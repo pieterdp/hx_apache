284626de969e47dc2701ee9c5c8fdb522f9fba86
Pieter De Praetere <pieter@packed.be>
2017-03-07 12:20:43 +0100

    Removed enable_php (doesn't work yet)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,10 @@
 ##
 # Default options for the Apache web server
 class hx_apache (
-    String $ssl_cipher = $hx_apache::params::ssl_cipher,
-    Array $ssl_protocol = $hx_apache::params::ssl_protocol,
-    Array $default_mods = $hx_apache::params::default_mods,
-    Strin $server_admin = $hx_apache::params::server_admin,
-    Boolean $enable_php = $hx_apache::params::enable_php
+    $ssl_cipher = $hx_apache::params::ssl_cipher,
+    $ssl_protocol = $hx_apache::params::ssl_protocol,
+    $default_mods = $hx_apache::params::default_mods,
+    $server_admin = $hx_apache::params::server_admin
 ) inherits hx_apache::params {
 
     include hx_apache::install
