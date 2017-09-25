55b3374197be602a91d1442e6698cd5d4497b870
Pieter De Praetere <pieter.de.praetere@helptux.be>
2017-09-25 21:28:04 +0200

    Updated cipher list, conformed to https://mozilla.github.io/server-side-tls/ssl-config-generator/

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,51 +3,54 @@
 # also ensures the web server is installed.
 class hx_apache::config () inherits hx_apache {
 
-    class {'apache':
-        purge_vhost_dir  => true,
-        default_vhost    => false,
-        default_mods     => $hx_apache::default_mods,
-        serveradmin      => $hx_apache::server_admin,
-        server_signature => false,
-        mpm_module       => 'prefork',
-    }
+  class { 'apache':
+    purge_vhost_dir  => true,
+    default_vhost    => false,
+    default_mods     => $hx_apache::default_mods,
+    serveradmin      => $hx_apache::server_admin,
+    server_signature => false,
+    mpm_module       => 'prefork',
+  }
 
-    ##
-    # Support for HTTP and HTTPS
-    apache::listen {'80': }
-    apache::listen {'443': }
+  ##
+  # Support for HTTP and HTTPS
+  apache::listen { '80': }
+  apache::listen { '443': }
 
-    ##
-    # We frequently use apache as a front-end for applications
-    # running on non-standard  ports. So enable mod_proxy,
-    # proxy_connect (for SSL) & mod_proxy_http
-    class {'apache::mod::proxy': }
-    class {'apache::mod::proxy_http': }
-    apache::mod {'proxy_connect': }
+  ##
+  # We frequently use apache as a front-end for applications
+  # running on non-standard  ports. So enable mod_proxy,
+  # proxy_connect (for SSL) & mod_proxy_http
+  class { 'apache::mod::proxy': }
+  class { 'apache::mod::proxy_http': }
+  apache::mod { 'proxy_connect': }
 
-    ##
-    # Secure SSL configuration (based on https://ssllabs.com)
-    class {'apache::mod::ssl':
-        ssl_cipher           => $hx_apache::ssl_cipher,
-        ssl_honorcipherorder => true,
-        ssl_protocol         => $hx_apache::ssl_protocol,
-        ssl_compression      => false,
-    }
+  ##
+  # Secure SSL configuration (based on https://ssllabs.com)
+  class { 'apache::mod::ssl':
+    ssl_cipher                 => $hx_apache::ssl_cipher,
+    ssl_honorcipherorder       => true,
+    ssl_protocol               => $hx_apache::ssl_protocol,
+    ssl_compression            => false,
+    ssl_stapling               => true,
+    ssl_stapling_return_errors => false,
+    stapling_cache             => '/var/run/ocsp(128000)'
+  }
 
-    ##
-    # Some applications contain a .git directory.
-    # This directory frequently contains
-    # sensitive information. We deny access to it.
-    apache::custom_config {'git':
-        content => '<Directory ~ "\.git">
+  ##
+  # Some applications contain a .git directory.
+  # This directory frequently contains
+  # sensitive information. We deny access to it.
+  apache::custom_config { 'git':
+    content => '<Directory ~ "\.git">
         Require all denied
 </Directory>',
-    }
+  }
 
-    ##
-    # Enable php if $enable_php is true
-    if $hx_apache::enable_php == true {
-        include hx_apache::config::php
-    }
+  ##
+  # Enable php if $enable_php is true
+  if $hx_apache::enable_php == true {
+    include hx_apache::config::php
+  }
 
 }
